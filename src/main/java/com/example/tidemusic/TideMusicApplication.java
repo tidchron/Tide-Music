5fd7bb1c7dda3cf0a83012ea19afb1e2557e09f7
tidchron <tidchron@icloud.com>
2025-03-05 17:26:58 +0900

    ✨ Feat: Spotify에서 accessToken 받아오기
    * SpotifyTokenManager 클래스 추가
    * SPOTIFY_CLIENT_ID와 SPOTIFY_CLIENT_SECRET 사용해서 accessToken 받아오기
    * .env 사용해서 환경변수 관리

--- src/main/java/com/example/tidemusic/TideMusicApplication.java
+++ src/main/java/com/example/tidemusic/TideMusicApplication.java
@@ -1,21 +1,33 @@
 package com.example.tidemusic;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.hc.core5.http.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Artist;
+import se.michaelthelin.spotify.requests.data.artists.GetArtistRequest;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class TideMusicApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException, SpotifyWebApiException {
+
         //.env 파일 불러오기
         Dotenv dotenv = Dotenv.load();
+
         //시스템 속성으로 전달
         System.setProperty("DB_URL", dotenv.get("DB_URL"));
         System.setProperty("DB_USER", dotenv.get("DB_USER"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(TideMusicApplication.class, args);
-    }
 
+        //SpotifyTokenManager 클래스에서 accessToken 받아오기
+        String accessToken = SpotifyTokenManager.getAccessToken();
+        System.out.println("accessToken = " + accessToken);
+    }
 }
